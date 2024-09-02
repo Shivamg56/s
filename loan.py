c20bccf15cea1623a1116680fa11a5a10876a9a1
Shivamg56 <shivamgupta5657@gmail.com>
2024-09-02 23:00:29 +0530

    Delete loan.py

--- loan.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.title('loan calculater')
-amount=st.number_input('needed amount')
-year=st.text_input('enter years')
-interestrate=st.write('interest rate per annum=8%')
-for
